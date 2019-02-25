9c74a215d837db699729f53d05d7c977f73ed38a
Neto, Mario Ferreira <mario.ferreira.neto@accenture.com>
2019-02-25 16:10:06 -0300

    Commitando primeiro tópico

--- ExercicioOrdenador/src/Ordenador.java
+++ ExercicioOrdenador/src/Ordenador.java
@@ -1,19 +1,22 @@
 import java.util.Collection;
 
 public class Ordenador implements IOrdenador{
+	
+	double maiorValor = 0.0;
 
 	@Override
 	public void inserirColecao(Collection<String> pColecao) {
 		
-		double maiorValor = 0.0;
-		
-		for(String valor : pColecao) {
-			double valorDouble = Double.parseDouble(valor);
-			if(valorDouble > maiorValor){
-				maiorValor = valorDouble;
-			}
+		try {
+			for(String valor : pColecao) {
+				double valorDouble = Double.parseDouble(valor);
+				if(valorDouble > maiorValor){
+					maiorValor = valorDouble;
+				}
+			}	
+		}catch(NumberFormatException n) {
+			n.printStackTrace();
 		}
-		
 	}
 
 }
--- ExercicioOrdenador/tst/TesteOrdenador.java
+++ ExercicioOrdenador/tst/TesteOrdenador.java
@@ -1,9 +1,59 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.*;
 public class TesteOrdenador {
 	
+	Ordenador o;
+	
+	@Before
+	public void Ordenador() {
+		o = new Ordenador();
+	};
+	
 	@Test
-	public void testarInserirColecao() {
+	public void testarInserirColecaoComNumerosInteiros() {
+		//Arranjar
+		Collection<String> colecao = new ArrayList<String>();
+		colecao.add("1");
+		colecao.add("2");
+		colecao.add("3");
 		
+		//Agir
+		o.inserirColecao(colecao);
+		
+		//Afirmar
+		assertEquals(3, o.maiorValor);
+	}
+	
+	@Test
+	public void testarInserirColecaoComNumerosReais() {
+		//Arranjar
+		Collection<String> colecao = new ArrayList<String>();
+		colecao.add("10.12");
+		colecao.add("20.2");
+		colecao.add("30.1");
+		
+		//Agir
+		o.inserirColecao(colecao);
+		
+		//Afirmar
+		assertEquals(30.1, o.maiorValor);
+	}
+	
+	@Test
+	public void testarInserindoValoresNaoNumericos() {
+		//Arranjar
+		Collection<String> colecao = new ArrayList<String>();
+		colecao.add("a");
+		colecao.add("b");
+		colecao.add("c");
+		
+		//Agir
+		o.inserirColecao(colecao);
+
 	}
 
 }
